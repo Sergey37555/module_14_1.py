044e6a72a4c17d1216d73bef62c62b292ae48a7e
Сергей Нефёдов <24twister555@mail.ru>
2024-12-16 16:30:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+i = 0
+for j in range(10, 101, 10):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                    (f'User{i+1}', f'example{i+1}@gmail.com', j, '1000'))
+    i += 1
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = 500 WHERE id = ?',(f'{i}',))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+users = cursor.fetchall()
+
+for user in users:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} |  Баланс: {user[3]}')
+
+connection.commit()
+connection.close()
